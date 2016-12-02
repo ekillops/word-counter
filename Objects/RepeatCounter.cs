e356f936a4aad5b3a27bec1b5d6721cbf7e5ee34
Erik Killops <erik.killops@gmail.com>
2016-12-02 11:12:21 -0800

    Add overloaded version of CountRepeats with bool caseSensitive parameter. Pass fourth spec test

--- Objects/RepeatCounter.cs
+++ Objects/RepeatCounter.cs
@@ -14,7 +14,26 @@ namespace WordCounter.Objects
       List<string> phraseWords = new List<string>(inputPhrase.Split(' '));
 
       foreach (string word in phraseWords) {
-        if (word.ToLower() == inputWord.ToLower())
+        if (word == inputWord)
+        {
+          wordInstanceCount += 1;
+        }
+      }
+      return wordInstanceCount;
+    }
+
+    public static int CountRepeats(string inputWord, string inputPhrase, bool caseSensitive)
+    {
+      int wordInstanceCount = 0;
+      inputWord = (caseSensitive) ? inputWord : inputWord.ToLower();
+
+      List<string> phraseWords = new List<string>(inputPhrase.Split(' '));
+
+      foreach (string word in phraseWords) {
+
+        string checkWord = (caseSensitive) ? word : word.ToLower();
+
+        if (checkWord == inputWord)
         {
           wordInstanceCount += 1;
         }
--- Tests/WordCounterTests.cs
+++ Tests/WordCounterTests.cs
@@ -9,10 +9,17 @@ namespace WordCounterTests
     [Theory]
     [InlineData("a", "a", 1)] // Spec 1 - Count instances of a single character in a single character phrase
     [InlineData("cat", "the fat cat", 1)] // Spec 2 - Count instances of a multi-character word in a phrase
-    [InlineData("cat", "the Fat CaT", 1)] // Spec 3 - Ignore case in phrase when counting
-    public void CountRepeatTheory(string word, string phrase, int expectedResult)
+    public void CountRepeatsTheory(string word, string phrase, int expectedResult)
     {
       Assert.Equal(expectedResult, RepeatCounter.CountRepeats(word, phrase));
+    }
+
+    [Theory]
+    [InlineData("cat", "the Fat CaT", false, 1)] // Spec 3 - Ignore case in phrase when counting
+    [InlineData("South", "south of South America", true, 1)] // Spec 4 - Do not ignore case in phrase if user chooses
+    public void CountRepeats_CaseSensitiveTheory(string word, string phrase, bool matchCase, int expectedResult)
+    {
+      Assert.Equal(expectedResult, RepeatCounter.CountRepeats(word, phrase, matchCase));
     }
   }
 }
